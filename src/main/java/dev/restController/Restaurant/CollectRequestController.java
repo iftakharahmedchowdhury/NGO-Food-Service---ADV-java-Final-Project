96d47fecf77474f7915ca0d18b43d622328891c1
iftakharahmedchowdhury <iftakharahmedchowdhuryimacc@gmail.com>
2023-12-28 23:13:42 +0600

    updating

--- src/main/java/dev/restController/Restaurant/CollectRequestController.java
+++ src/main/java/dev/restController/Restaurant/CollectRequestController.java
@@ -1,12 +1,8 @@
 package dev.restController.Restaurant;
 
-import dev.domain.Admin.CollectRequestWithItems;
 import dev.domain.Restaurant.CollectRequest;
-import dev.domain.Restaurant.CollectRequestsFoodItem;
-import dev.domain.User.User;
 import dev.service.Restaurant.CollectRequestItemService;
 import dev.service.Restaurant.CollectRequestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
